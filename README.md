56bda8bbde0c53b7d46ba2502cc0cd5f5ec9ad14
Fernando O. Gallego <f.ortega.gallego@gmail.com>
2020-05-01 16:44:45 +0200

    Added Freeling transformed resources, base notebook and pandas profiling report.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Andaluh-eda
+Exploratory Data Analysis for transliteration data between castilian spanish and andaluh.
+
+## Data
+File ``entries_es.txt`` was obtained from [Freeling Spanish dictionaries](https://github.com/TALP-UPC/FreeLing/tree/master/data/es/dictionary/entries), which are under a [Lesser General Public License For Linguistic Resources](https://spdx.org/licenses/LGPLLR.html) license.
+
+The modifications made to those linguistic resources comprised the creation of a unique list of Spanish words without any other related information.
+
+File ``entries_es_and.csv`` relies on a transliteration from previous file and is also under the same license.
---
+++ mappings.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from andaluh import epa
+import time
+import codecs
+from multiprocessing import Pool
+
+ENTRIES_FILE = 'tests/entries_es.txt'
+MAPPING_CSV = 'tests/entries_es_and2.csv'
+# ENTRIES_FILE = 'tests/entries_out_mini.txt'
+# MAPPING_CSV = 'tests/entries_mapping_mini.csv'
+
+
+if __name__ == '__main__':
+    vafs = [u'ç', u's', u'z', u'h']
+    vvfs = [u'j', u'h']
+
+    t_init = time.time()
+    pool = Pool()
+    with codecs.open(ENTRIES_FILE, encoding='utf-8', mode='r') as f_entries:
+        content = f_entries.read().replace("\r\n\r\n", "\r\n")
+        and_variants_results_pool = [pool.apply_async(epa, args=(content, vaf, vvf)) for vaf in vafs for vvf in vvfs]
+        and_variants_results = [result.get() for result in and_variants_results_pool]
+
+        matrix_result = [', '.join(row) for row in zip(*[content.split("\r\n")] + [and_variant_result.split("\r\n")
+                                                                                   for and_variant_result in
+                                                                                   and_variants_results])]
+        with codecs.open(MAPPING_CSV, encoding='utf-8', mode='w') as f_mapping:
+            f_mapping.write(u'cas, and_çj, and_sj, and_zj, and_hj, and_çh, and_sh, and_zh, and_hh\n')
+            for row_str in matrix_result:
+                f_mapping.write("%s\n" % row_str)
+    t_total1 = time.time() - t_init
+    print('Time: {}'.format(t_total1))
